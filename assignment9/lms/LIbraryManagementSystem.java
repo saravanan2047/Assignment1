aef525c90f415f3856c5da7c2ae384228cee766e
SARAVANAN <95707512+saravanan2047@users.noreply.github.com>
2024-03-27 11:16:11 +0530

    Update LIbraryManagementSystem.java

--- assignment9/lms/LIbraryManagementSystem.java
+++ assignment9/lms/LIbraryManagementSystem.java
@@ -1,5 +1,5 @@
 package assignment9.lms;
-
+import java.util.*;
 public class LIbraryManagementSystem 
 {
 	private String UserType;
@@ -8,6 +8,8 @@ public class LIbraryManagementSystem
 	private User user= new User();
 	private Book book= new Book();
 	private Librarian librarian= new Librarian();
+	private List<User> userList= new ArrayList<>();
+	Private List<Book>bookList= new ArrayList<>();
 	
 	public void Login()
 	{
